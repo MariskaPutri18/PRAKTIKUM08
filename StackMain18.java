8b9a48891c3685ba392704c85e27c37de66553cb
MariskaPutri18 <mariskaputri960@gmail.com>
2024-04-03 13:39:46 +0700

    modif soal praktikum2

--- StackMain18.java
+++ StackMain18.java
@@ -15,7 +15,6 @@ public class StackMain18 {
             System.out.print("Pilih operasi (1/2/3/4): ");
             int operasi = sc.nextInt();
             sc.nextLine();
-
             switch (operasi) {
                 case 1:
                 System.out.print("jenis: ");
@@ -31,9 +30,6 @@ public class StackMain18 {
                 sc.nextLine();
 
                 Pakaian18 p = new Pakaian18(jenis, warna, merk, ukuran, harga);
-                System.out.print("apakah Anda akan menambhakan data baru ke stack (y/n)? ");
-                pilih = sc.next().charAt(0);
-                sc.nextLine();
                 stk.push(p);
                     break;
                 case 2:
@@ -50,7 +46,6 @@ public class StackMain18 {
                 default:
                     System.out.println("Operasi tidak ada");
             }
-    
             System.out.print("apakah Anda akan menambhakan data baru ke stack (y/n)? ");
             pilih = sc.next().charAt(0);
             sc.nextLine();
